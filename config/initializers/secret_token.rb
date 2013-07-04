8ae46efc257bf6c11d0d5d05ca530309b941de73
Paulo Roncon <proncon@gmail.com>
2013-07-04 15:16:11 +0100

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZeroDay::Application.config.secret_token = 'c16e42b530d7192da9e7e6f1f8356db81fb5d6706f521bf503255234dc166849baa8eb93fba2a0cc79359891110de1fab8c5b7e729037b81c8e17b7460af8839'
